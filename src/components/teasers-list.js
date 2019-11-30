9b402b36162f95f0dd6456e07f57c3d565c9e55c
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2019-11-29 19:00:13 -0800

    feat: show basic-pages at home page (#13)

--- src/components/teasers-list.js
+++ src/components/teasers-list.js
@@ -3,11 +3,25 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 import Card from "gatsby-theme-octahedroid/src/components/card";
 
-// fetch all content from graphql and render it with card component
 const TeasersList = () => {
-  const items = [];
   const posts = useStaticQuery(graphql`
     {
+      allNodePage {
+        edges {
+          node {
+            path {
+              alias
+            }
+            relationships {
+              node_type {
+                name
+              }
+            }
+            title
+          }
+        }
+      }
+
       allNodeArticle {
         edges {
           node {
@@ -20,28 +34,38 @@ const TeasersList = () => {
               }
             }
             title
-            field_teaser
           }
         }
       }
     }
   `);
+
   return (
     <div className="bg-lightShade">
       <div className="container mx-auto my-4 flex">
-        {posts &&
-          posts.allNodeArticle.edges.map(({ node }) => (
-            <div className="w-full lg:w-1/3">
+        {posts && posts.allNodePage.edges.map(({ node }) => (
+              <div className="w-full lg:w-1/2">
+                <Card
+                  intro={node.relationships.node_type.name}
+                  title={node.title}
+                  image="hero.png"
+                  link={node.path.alias}
+                  ctaText="Read more..."
+                />
+              </div>
+        ))}
+
+        {posts && posts.allNodeArticle.edges.map(({ node }) => (
+            <div className="w-full lg:w-1/2">
               <Card
                 intro={node.relationships.node_type.name}
                 title={node.title}
                 image="hero.png"
                 link={node.path.alias}
-                text={node.field_teaser}
                 ctaText="Read more..."
               />
             </div>
-          ))}
+        ))}
       </div>
     </div>
   );
